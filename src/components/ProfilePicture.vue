a5506de478d74e32e87cda17922dbd0eb7f6c280
Diogo Loiola <diogomouraloiola@gmail.com>
2022-05-26 08:27:06 -0300

    create profilePicture componente to form

---
+++ src/components/ProfilePicture.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    <img src="@/assets/images/Profile.png" alt="imagem de perfil">
+  </section>
+</template>
+
+<style scoped>
+.container {
+  width: 183px;
+  height: 185px;
+  border-radius: 10px;
+}
+</style>
