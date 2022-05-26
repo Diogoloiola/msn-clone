32ef4b449b23c8dda3a11daae3d971ae44f8335e
Diogo Loiola <diogomouraloiola@gmail.com>
2022-05-26 15:31:32 -0300

    create route and for dashboard route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const Login = () => import("../views/Login/index.vue");
+const Home = () => import("../views/Home/index.vue");
+
 
 export const routes = [
   {
     path: "/",
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: Home,
   },
   {
     path: "/:pathMatch(.*)*",
