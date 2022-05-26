a90e2be7da681a49d4c095d52600a2a7d4ae4af8
Diogo Loiola <diogomouraloiola@gmail.com>
2022-05-26 07:18:33 -0300

    create componente for animation login

---
+++ src/components/AnimationLogin.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="images">
+    <img src="@/assets/images/Icone Messenger.png" alt="logo msn" />
+    <div>
+      <h3 class="title">Windows Live</h3>
+      <p class="subtile">Messenger</p>
+      <p class="subtitle-two mt-2">Iniciando...</p>
+    </div>
+  </div>
+</template>
+<style scoped>
+.images {
+  width: 50%;
+  display: flex;
+  align-items: center;
+}
+.title {
+  font-family: "Segoe UI";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 43px;
+  color: #262626;
+}
+.subtile {
+  font-family: "Segoe UI";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 64px;
+  line-height: 85px;
+
+  color: rgba(38, 38, 38, 0.79);
+}
+.subtile-two {
+  font-family: "Segoe UI";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 21px;
+
+  color: #383838;
+}
+</style>
