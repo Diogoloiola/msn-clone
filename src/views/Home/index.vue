31481b87f270bc0edfe2098c6b187e597b8f4aec
Diogo Loiola <diogomouraloiola@gmail.com>
2022-05-31 10:34:09 -0300

    create new classes for global style

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="container-fluid">
     <custom-header></custom-header>
-    <div
-      style="
-        background: linear-gradient(64.31deg, #58d4f9 2.8%, #097bdb 97.47%);
-        height: 20%;
-      "
-    >
+    <div class="bg-blue-gradient h-20">
       <div class="container">
         <div class="d-flex align-items-center">
           <img
-            style="margin-top: 20px"
-            class="picture"
+            class="picture mt-2"
             src="@/assets/images/Group 6.png"
             alt="foto de perfil do usuário"
           />
@@ -20,13 +14,13 @@
             <p class="mt-1">UI Designer</p>
           </div>
         </div>
-        <div style="margin-top: 30px">
+        <div class="mt-2">
           <custom-input
             :inputType="'text'"
             :placeholder="'Pesquisar contatos na Web'"
           ></custom-input>
         </div>
-        <div class="mt-1" style="margin-top: 30px">
+        <div class="mt-3" >
           <img
             class=""
             src="@/assets/images/Favoritos.png"
@@ -42,25 +36,11 @@
         </div>
       </div>
       <footer class="bg-gray">
-        <div class="container" style="width: 100%; height: 320px">
-          <div
-            style="
-              display: flex;
-              height: 200px;
-              align-items: center;
-              margin-left: 30px;
-            "
-          >
+        <div class="container w-100" style="height: 320px">
+          <div class="align-items-center d-flex mt-3" style="height: 200px">
             Novidades
           </div>
-          <div
-            style="
-              width: 100%;
-              height: 2px;
-              background: #c4c4c4;
-              box-shadow: -3px 0px 6px rgba(0, 0, 0, 0.4);
-            "
-          ></div>
+          <div class="line-footer"></div>
         </div>
       </footer>
     </div>
@@ -82,15 +62,19 @@
   height: 90px;
 }
 
-span {
-  font-weight: 200;
-}
-ul {
-  list-style-type: none;
+.bg-blue-gradient {
+  background: linear-gradient(64.31deg, #58d4f9 2.8%, #097bdb 97.47%);
 }
 
 .bg-gray {
   background: linear-gradient(180deg, #eff8fa 0%, #e9f5fc 47.79%, #d0e8f2 100%);
+}
+
+.line-footer {
+  width: 100%;
+  height: 2px;
+  background: #c4c4c4;
+  box-shadow: -3px 0px 6px rgba(0, 0, 0, 0.4);
 }
 </style>
 
@@ -108,13 +92,15 @@ const data = reactive({
     {
       id: 2,
       src: "src/assets/images/Débora Pereira.png",
-    },{
+    },
+    {
       id: 3,
       src: "src/assets/images/Débora Pereira.png",
-    },{
+    },
+    {
       id: 4,
       src: "src/assets/images/Débora Pereira.png",
-    }
+    },
   ],
 });
 </script>
