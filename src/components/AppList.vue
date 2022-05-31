46c62ca815590d098878a36854f3783be578ed82
Diogo Loiola <diogomouraloiola@gmail.com>
2022-05-31 10:09:55 -0300

    create component to render horizontal list

---
+++ src/components/AppList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="list">
+    <li v-for="item in list" :key="item.id">{{ item.value }}</li>
+  </ul>
+</template>
+<style scope>
+.list {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+}
+
+.list li {
+  padding: 8px;
+}
+</style>
+
+<script setup>
+const { list } = defineProps({
+  list: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
