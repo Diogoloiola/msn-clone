e29412c14e8d06bf0d12c6355dbc325abc945ffe
Diogo Loiola <diogomouraloiola@gmail.com>
2022-05-31 10:19:09 -0300

    add custom-head to home page

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,21 +1,6 @@
 <template>
   <div class="container-fluid">
-    <header class="header d-flex bg-blue justify-content-between">
-      <div class="ml-1 justify-content-between align-items-center d-flex">
-        <img src="@/assets/images/mini_logo.png" alt="logo msn" />
-        <h3 class="ml-1">Windows Live Messenger</h3>
-      </div>
-      <div>
-        <img src="@/assets/images/buttons.png" alt="buttons" />
-      </div>
-    </header>
-    <div class="bg-blue" style="border-top: 1px solid #a7b1b6">
-      <div class="container">
-        <div style="width: 20%">
-          <app-list :list="data.list"></app-list>
-        </div>
-      </div>
-    </div>
+    <custom-header></custom-header>
     <div
       style="
         background: linear-gradient(64.31deg, #58d4f9 2.8%, #097bdb 97.47%);
@@ -155,36 +140,5 @@ ul {
 
 <script setup>
 import CustomInput from "@/components/CustomInput.vue";
-import AppList from "../../components/AppList.vue";
-
-import { reactive } from "@vue/reactivity";
-
-const data = reactive({
-  list: [
-    {
-      id: 1,
-      value: "Arquivos",
-    },
-    {
-      id: 2,
-      value: "Contatos",
-    },
-    {
-      id: 3,
-      value: "Ações",
-    },
-    {
-      id: 4,
-      value: "Ferramentas",
-    },
-    {
-      id: 5,
-      value: "Ajuda",
-    },
-    {
-      id: 6,
-      value: "Plus",
-    },
-  ],
-});
+import CustomHeader from "./CustomHeader.vue";
 </script>
